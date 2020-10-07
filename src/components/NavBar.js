962bdc51b0e59036c2d48cdcf3c223ab8b2d41bf
Bogdan Matuszyczk <bogdan.utn@gmail.com>
2020-10-06 22:10:40 -0300

    add navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import {MenuIcon} from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function NavBar() {
+export default function ButtonAppBar() {
   const classes = useStyles();
 
   return (
